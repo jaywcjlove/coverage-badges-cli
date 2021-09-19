5a91855cc28232248219addc704055c35471d07a
jaywcjlove <398188662@qq.com>
2021-09-20 05:32:19 +0800

    test: Add test case.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,24 @@
 import FS from 'fs-extra';
 import path from 'path';
-import run from '../src/index'
+import run from '../src';
 
-it('sum test case', async () => {
+it('test case', async () => {
+  const summary = path.resolve(process.cwd(), 'coverage/coverage-summary.json');
+  FS.ensureDirSync(path.dirname(summary));
+  FS.writeJsonSync(summary, {
+    "total": {
+      "statements": {
+        "total": 45,
+        "covered": 25,
+        "skipped": 0,
+        "pct": 55.56
+      },
+    },
+  })
   expect(run()).toBeUndefined();
-  const output = path.join(process.cwd(), 'coverage/badges.svg');
-  expect(FS.existsSync(output)).toBeTruthy();
+  const output = path.resolve(process.cwd(), 'coverage/badges.svg');
+  const exists = FS.existsSync(output);
+  expect(exists).toBeTruthy();
   const dirs = await FS.readdir(path.join(process.cwd(), 'coverage'));
   expect(dirs).toEqual(['badges.svg', 'coverage-summary.json', 'lcov-report', 'lcov.info']);
 });
