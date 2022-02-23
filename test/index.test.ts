80bc52246123d74ccef99072b9749d543f05c9cd
jaywcjlove <398188662@qq.com>
2022-02-23 22:16:52 +0800

    test: fix test case errors.

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,6 +5,7 @@ import { badge } from '../src/badges';
 import { create } from '../src/create';
 import { cliHelp, exampleHelp } from '../src';
 
+
 it('test run case', async () => {
   const summary = path.resolve(process.cwd(), 'coverage/coverage-summary.json');
   FS.ensureDirSync(path.dirname(summary));
@@ -28,30 +29,7 @@ it('test run case', async () => {
 });
 
 it('test badge case', async () => {
-  const svgStr = `
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="104" height="20" role="img" aria-label="coverage: 100%">
-  <title>coverage: 100%</title>
-  <linearGradient id="s" x2="0" y2="100%">
-    <stop offset="0" stop-color="#bbb" stop-opacity=".1" />
-    <stop offset="1" stop-opacity=".1" />
-  </linearGradient>
-  <clipPath id="r">
-    <rect width="104" height="20" rx="3" fill="#fff" />
-  </clipPath>
-  <g clip-path="url(#r)">
-    <rect width="61" height="20" fill="#555"/>
-    <rect x="61" width="143" height="20" fill="#49c31a"/>
-    <rect width="104" height="20" fill="url(#s)"/>
-  </g>
-  <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
-    <text aria-hidden="true" x="315" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="510">coverage</text>
-    <text x="315" y="140" transform="scale(.1)" fill="#fff" textLength="510">coverage</text>
-    <text aria-hidden="true" x="815" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" >100%</text>
-    <text x="815" y="140" transform="scale(.1)" fill="#fff" >100%</text>
-  </g>
-</svg>
-  `
-  const str = badge({ style: 'flat', _: [] }, {
+  const str = badge({ style: 'flat' }, {
     "total": {
       "statements": {
         "total": 45,
@@ -61,11 +39,18 @@ it('test badge case', async () => {
       },
     },
   } as any);
-  expect(str).toEqual(svgStr);
+  expect(str.indexOf(`<text x="648" y="138" textLength="330">100%</text`) > 0).toBeTruthy();
+  expect(str.indexOf(`<text x="60" y="148" textLength="503" fill="#000" opacity="0.1">coverage</text>`) > 0).toBeTruthy();
 });
 
+console.log = jest.fn();
 it('test create case', async () => {
   const str = create({ style: 'flat', _: [], source: 'coverage2/coverage-summary.json', output: 'coverage2/svg.svg' });
+
+  // @ts-ignore
+  expect(console.log.mock.calls[0][0]).toBe('\x1b[31mErr Source Path:\x1b[0m coverage2/coverage-summary.json\n');
+  // @ts-ignore
+  expect(console.log.mock.calls[0][1]).toBe('> please specify the file directory\n');
   expect(str).toBeUndefined();
 });
 
