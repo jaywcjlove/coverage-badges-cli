94f941e2fb8409a357be6024350d8a6c5aeda732
jaywcjlove <398188662@qq.com>
2022-02-23 22:43:43 +0800

    fix: output errror log.

--- src/action.ts
+++ src/action.ts
@@ -13,9 +13,10 @@ import { Summary } from './create';
     fs.ensureDirSync(path.dirname(output));
     if (!fs.existsSync(source)) {
       setFailed(`File \x1b[31m${source}\x1b[0m does not exist.\n please specify the file directory\n\x1b[35mnpm\x1b[0m coverage-badges-cli \x1b[33m--source\x1b[0m coverage/coverage-summary.json`);
+      return
     }
-    info(`\nSource Path: \x1b[32;1m${source}\x1b[0m\n`);
-    info(`\nOutput Path: \x1b[32;1m${output}\x1b[0m\n`);
+    info(`Source Path: \x1b[32;1m${source}\x1b[0m`);
+    info(`Output Path: \x1b[32;1m${output}\x1b[0m`);
     const sourceData: Summary = require(source);
     const svgStr = badge({ label, style }, sourceData);
     fs.writeFileSync(output, svgStr);
