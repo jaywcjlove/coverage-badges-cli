83ee80795389c54baf97f87fd97ee3ac5a2cef52
jaywcjlove <398188662@qq.com>
2022-02-23 22:51:17 +0800

    fix: add output log.

--- src/action.ts
+++ src/action.ts
@@ -1,15 +1,15 @@
 import path from 'path';
 import fs from 'fs-extra';
-import { setFailed, getInput, info, error } from '@actions/core';
+import { setFailed, getInput, info, startGroup, endGroup } from '@actions/core';
 import { badge, BadgeOption } from './badges';
 import { Summary } from './create';
 
 ;(async () => {
   try {
-    const output = path.resolve(getInput('output') || 'coverage/badges.svg');
+    const output = path.resolve(process.cwd(), getInput('output') || 'coverage/badges.svg');
+    const source = path.resolve(process.cwd(), getInput('source') || 'coverage/coverage-summary.json');
     const label = getInput('label') || 'coverage';
     const style = (getInput('style') || 'classic') as BadgeOption['style'];
-    const source = path.resolve(getInput('source') || 'coverage/coverage-summary.json');
     fs.ensureDirSync(path.dirname(output));
     if (!fs.existsSync(source)) {
       setFailed(`File \x1b[31m${source}\x1b[0m does not exist.\n please specify the file directory\n\x1b[35mnpm\x1b[0m coverage-badges-cli \x1b[33m--source\x1b[0m coverage/coverage-summary.json`);
@@ -17,7 +17,12 @@ import { Summary } from './create';
     }
     info(`Source Path: \x1b[32;1m${source}\x1b[0m`);
     info(`Output Path: \x1b[32;1m${output}\x1b[0m`);
+    
     const sourceData: Summary = require(source);
+    startGroup(`Source Path: \x1b[32;1m${source}\x1b[0m`);
+    info(`${JSON.stringify(sourceData, null, 2)}`);
+    endGroup();
+    
     const svgStr = badge({ label, style }, sourceData);
     fs.writeFileSync(output, svgStr);
     info(`\nCreate Coverage Badges: \x1b[32;1m${path.relative(process.cwd(), output)}\x1b[0m\n`);
